6ec704ff8f6d87332366ad21a0e7464fa50ece20
Shrawak Hack <shrawakagni@gmail.com>
2025-04-23 06:38:46 +0000

    changed nextcinfig

--- kangen_waterproj/next.config.ts
+++ kangen_waterproj/next.config.ts
@@ -1,13 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-// next.config.js
-module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+export default nextConfig;
+
